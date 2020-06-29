b81e12350fd9e61f47be410fea3304adceab7ac5
Johan Svensson <johanksvensson@hotmail.com>
2020-06-29 17:39:20 +0200

    Add complete Spotify Parser.

---
+++ Awesome.FeedParser/Models/SpotifyFeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Awesome.FeedParser.Models
+{
+    /// <summary>
+    /// Spotify Parser Feed Result Class.
+    /// </summary>
+    public class SpotifyFeed
+    {
+        /// <summary>
+        /// Number of concurrent episodes (items) to display.
+        /// </summary>
+        public int? Limit { get; internal set; }
+
+        /// <summary>
+        /// Podcast country of origin ISO 3166 code.
+        /// </summary>
+        public RegionInfo? CountryOfOrigin { get; internal set; }
+    }
+}
--- Awesome.FeedParser/Parsers/SpotifyParser.cs
+++ Awesome.FeedParser/Parsers/SpotifyParser.cs
@@ -1,24 +1,115 @@
-﻿using Awesome.FeedParser.Interfaces;
+﻿using Awesome.FeedParser.Extensions;
+using Awesome.FeedParser.Interfaces;
 using Awesome.FeedParser.Models;
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 using System.Xml;
 
 namespace Awesome.FeedParser.Parsers
 {
+    /// <summary>
+    /// Parser for Spotify feed nodes.
+    /// </summary>
     internal sealed class SpotifyParser : BaseParser
     {
+        //Spotify Namespace URI
         public static string Namespace { get; } = @"https://www.spotify.com/ns/rss";
 
+        //Parser lazy loaded instance
         public static Lazy<IParser> Instance { get; } = new Lazy<IParser>(() => new SpotifyParser());
 
+        //Private constructor to prevent external initalization
         private SpotifyParser()
         {
         }
 
-        public override Task<bool> Parse(XmlReader reader, Feed feed)
+        /// <summary>
+        /// Main Spotify parsing method.
+        /// </summary>
+        /// <param name="reader">Current xml feed reader.</param>
+        /// <param name="feed">Current feed result.</param>
+        /// <returns></returns>
+        public override async Task<bool> Parse(XmlReader reader, Feed feed)
         {
-            return Task.FromResult(false);
+            //Init
+            bool result;
+
+            //Verify Element Node
+            if (result = reader.NodeType == XmlNodeType.Element && (!reader.IsEmptyElement || reader.HasAttributes))
+            {
+                //Init
+                NodeInformation nodeInfo = reader.NodeInformation();
+                feed.Spotify ??= new SpotifyFeed();
+
+                //Add Spotify to feed content type.
+                ICommonFeed feedTarget = feed.CurrentItem ?? (ICommonFeed)feed;
+                feedTarget.ContentType |= FeedContentType.Spotify;
+
+                //Identify node name
+                switch (reader.LocalName)
+                {
+                    #region Feed
+
+                    case "limit": //Number of concurrent episodes (items) to display.
+                        {
+                            if (feed.Spotify != null)
+                            {
+                                //Get limit count
+                                var content = reader.GetAttribute("recentCount");
+                                if (content != null)
+                                {
+                                    if (int.TryParse(content, out var count))
+                                        feed.Spotify.Limit = count;
+                                    else
+                                        //Unknown node format
+                                        SetParseError(ParseErrorType.UnknownNodeFormat, nodeInfo, feed, content);
+                                }
+                                else
+                                {
+                                    //Missing count attribute
+                                    SetParseError(ParseErrorType.MissingAttribute, nodeInfo, feed, null, "recentCount");
+                                }
+                            }
+                            result = false;
+                        }
+                        break;
+
+                    case "countryOfOrigin": //Podcast country of origin ISO 3166 code.
+                        {
+                            if (feed.Spotify != null)
+                            {
+                                //Get Country of origin
+                                var content = await reader.ReadElementContentAsStringAsync();
+                                try
+                                {
+                                    //Attempt to set country of origin
+                                    feed.Spotify.CountryOfOrigin = new RegionInfo(content);
+                                }
+                                catch (ArgumentException ex)
+                                {
+                                    //Unknown node format
+                                    SetParseError(ParseErrorType.UnknownNodeFormat, nodeInfo, feed, content, ex.Message);
+                                }
+                            }
+                            else
+                                result = false;
+                            break;
+                        }
+
+                    #endregion Feed
+
+                    default: //Unknown feed/item node, continue to next.
+                        {
+                            SetParseError(ParseErrorType.UnknownNode, nodeInfo, feed);
+                            result = false;
+                            break;
+                        }
+                }
+            }
+
+            //Return result
+            return result;
         }
     }
 }
